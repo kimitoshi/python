f0b9c4a92ef4be80aae65861dbac7ea240d07713
Narita <kimitoshi_narita@hotmail.com>
2020-10-24 06:03:33 +0900

    add new file

---
+++ 3章関数/helloFunc.py
@@ -0,0 +1,8 @@
+def hello():
+    print('Hwdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+
+hello()
+hello()
+hello()
---
+++ 3章関数/samaName4.py
@@ -0,0 +1,6 @@
+def spam():
+    eggs = 'spam local'
+    print(eggs)
+   
+eggs = 'global'
+spam()
